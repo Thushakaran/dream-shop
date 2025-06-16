bab36cbad396089ec90f7adcdec2a3339a90e78c
Thushakaran <thushakarant@gmail.com>
2025-06-16 10:32:22 +0530

    create models

--- src/main/java/com/se/dream_shop/model/Category.java
+++ src/main/java/com/se/dream_shop/model/Category.java
@@ -1,4 +1,24 @@
 package com.se.dream_shop.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
