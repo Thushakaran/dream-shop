087bd89ff1a790b5ef68a675beb5a708511704c2
Thushakaran <thushakarant@gmail.com>
2025-06-16 14:57:40 +0530

    add IProductService, ProductService, ProductUpdateRequest, ResourceNotFoundException

---
+++ src/main/java/com/se/dream_shop/service/product/IProductService.java
@@ -0,0 +1,32 @@
+package com.se.dream_shop.service.product;
+
+import com.se.dream_shop.model.Product;
+import com.se.dream_shop.request.AddProductRequest;
+import com.se.dream_shop.request.ProductUpdateRequest;
+
+import java.util.List;
+
+public interface IProductService {
+    Product addProduct(AddProductRequest product);
+
+    Product getProductById(Long id);
+
+    void deleteProductById(Long id);
+
+    Product updateProduct(ProductUpdateRequest request, Long productId);
+
+    List<Product> getAllProducts();
+
+    List<Product> getProductByCategory(String category);
+
+    List<Product> getProductByBrand(String brand);
+
+    List<Product> getProductByCategoryAndBrand(String category, String brand);
+
+    List<Product> getProductByName(String name);
+
+    List<Product> getProductByBrandAndName(String brand, String name);
+
+    Long countProductsByBrandAndName(String brand, String name);
+
+}
